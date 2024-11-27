6082e0858c5f7aed597dd8dfd24e53b77a05f2f9
Marco Schwarz <marco.swz@hotmail.com>
2024-11-27 18:35:03 +0100

    Switch to rust

--- flake.nix
+++ flake.nix
@@ -2,25 +2,29 @@
     inputs = {
         nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
         flake-utils.url = "github:numtide/flake-utils";
+        rust-overlay.url = "github:oxalica/rust-overlay";
     };
 
-    outputs = {nixpkgs, flake-utils, ... }: 
-    flake-utils.lib.eachSystem flake-utils.lib.allSystems (system:
-    let
-        pkgs = import nixpkgs {
-            inherit system;
-        };
-    in {
-        devShell = (pkgs.buildFHSUserEnv {
-            name = "elixir-env";
-            targetPkgs = pkgs: (with pkgs; [
-                clang
-                elixir
-                next-ls
-                xmlstarlet
-            ]);
-            profile = "export ELIXIR_LS=${pkgs.elixir-ls}/lib";
-            runScript = "bash";
-        }).env;
-    });
+    outputs = { nixpkgs, flake-utils, rust-overlay, ... }:
+        flake-utils.lib.eachSystem [ "x86_64-linux" ] (system: 
+            let
+                pkgs = import nixpkgs {
+                    inherit system;
+                    overlays = [
+                        rust-overlay.overlays.default
+                    ];
+                };
+
+            in {
+                devShell = pkgs.mkShell {
+                    buildInputs = with pkgs; [ 
+                        cmake
+                        rust-analyzer
+                        rust-bin.stable.latest.default
+                        pkg-config
+                        openssl.dev
+                        rustup
+                    ];
+                };
+            });
 }
--- test/xml_parser_test.exs
+++
@@ -1,8 +0,0 @@
-defmodule XmlParserTest do
-  use ExUnit.Case
-  doctest XmlParser
-
-  test "greets the world" do
-    assert XmlParser.hello() == :world
-  end
-end
